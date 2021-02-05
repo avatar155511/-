5e6638fe2c3fdeb73065a0c988c470c16f3c473d
avatar155511 <78071959+avatar155511@users.noreply.github.com>
2021-02-05 20:52:37 +0400

    Add files via upload

---
+++ DZ4/DZ4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint a1 = 0;
+            uint b1 = 0;
+
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            
+            
+            
+            while (a < b)
+
+            {
+                a++;
+                Console.WriteLine(a);
+
+                if (a % 2 == 0)
+                {
+                    a1++;
+                }
+                else
+                {
+                    b1++;
+                }
+                
+                
+         
+            }
+            Console.WriteLine("Кол-во четных чисел"+"-"+ a1);
+            Console.WriteLine("Кол-во не четных чисел"+"-" + b1);
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ DZ5/DZ5/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину");
+            int Dlina = int.Parse(Console.ReadLine());
+            
+
+            for (int a = 0; a < Dlina; a++)
+            {
+                for (int b = 0; b <= a; b++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();        
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            for (int a = 0; a < Dlina; a++)
+            {
+                for (int b = Dlina; b > a; b--)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            for (int a = 0; a < Dlina; a++)
+            {
+                for (int b = Dlina; b > a; b--)
+                {
+                    Console.Write(" ");
+                }
+                for (int b = 0; b <= a; b++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            for (int a = 0; a < Dlina; a++)
+            {
+                for (int b = 0; b <= a; b++)
+                {
+                    Console.Write(" ");
+                }
+                for (int b = Dlina; b > a; b--)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
